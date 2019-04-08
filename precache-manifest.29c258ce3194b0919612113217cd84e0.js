8da001c939ba1e303fe8ea51e2c286c1201c68fd
kimi532 <kimi532@naver.com>
2019-04-08 21:39:10 +0900

    Updates

---
+++ precache-manifest.29c258ce3194b0919612113217cd84e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.e6fc6960.js"
+  },
+  {
+    "revision": "3fb757bdb0fa44b02891",
+    "url": "/movie_app/static/js/main.97b9380c.chunk.js"
+  },
+  {
+    "revision": "6303b1dc6d95fb6846a7",
+    "url": "/movie_app/static/js/2.9dee5afb.chunk.js"
+  },
+  {
+    "revision": "3fb757bdb0fa44b02891",
+    "url": "/movie_app/static/css/main.942d7de1.chunk.css"
+  },
+  {
+    "revision": "47278a583bea7dba2be084b0951dec6a",
+    "url": "/movie_app/index.html"
+  }
+];
