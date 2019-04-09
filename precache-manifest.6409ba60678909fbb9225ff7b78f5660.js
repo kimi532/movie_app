f91647012ab2450760ed5e5390c852181895df9c
kimi532 <kimi532@naver.com>
2019-04-09 17:19:04 +0900

    Updates

---
+++ precache-manifest.6409ba60678909fbb9225ff7b78f5660.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.e6fc6960.js"
+  },
+  {
+    "revision": "79ff9fd7a77ebe93039d",
+    "url": "/movie_app/static/js/main.b0027ac9.chunk.js"
+  },
+  {
+    "revision": "ae6b888c276a233d7d7a",
+    "url": "/movie_app/static/js/2.d92db21f.chunk.js"
+  },
+  {
+    "revision": "79ff9fd7a77ebe93039d",
+    "url": "/movie_app/static/css/main.942d7de1.chunk.css"
+  },
+  {
+    "revision": "1d6f3bb1310a3dbea52d2d5a8d166263",
+    "url": "/movie_app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/movie_app/precache-manifest.29c258ce3194b0919612113217cd84e0.js"
+  "/movie_app/precache-manifest.6409ba60678909fbb9225ff7b78f5660.js"
 );
 
 workbox.clientsClaim();
